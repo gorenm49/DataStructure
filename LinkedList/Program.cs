5458f517286223397fe1b228cf1d40c0ddc2adfd
Nitya <gorenityanand96@gmail.com>
2022-02-12 23:20:29 +0530

    [Nityanand]:LinkedList Operations Added

---
+++ LinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.InsertFuncAtPerticularPostion(2,40);
+            list.Display();
+            list.AddInReversrOrder(25);
+            list.Display();
+            list.RemoveFirstNode();
+            list.Display();
+            list.RemoveLastNode();
+            list.Display();
+            list.Search(56);
+            list.DeleteNodeAtPerticularPosition(2);
+            list.Display();
+            list.size();
+            list.Display();
+        }
+    }
+}
+
+
