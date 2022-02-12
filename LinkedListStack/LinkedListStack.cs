3baf1bd5f649631217cb1cc168e5cc2aaa982485
Nitya <gorenityanand96@gmail.com>
2022-02-13 00:03:59 +0530

    [Nityanand]:LinkedListStack Operations Added

---
+++ LinkedListStack/LinkedListStack.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace DataStructure
+{
+    public class LinkedListStack
+    {
+        public Node top;
+
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            System.Console.WriteLine(data+" pushed to stack.");
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                System.Console.WriteLine(temp.data+ " ");
+                temp = temp.next;
+            }  
+        }
+
+        public void Peek()  
+        {
+            if (this.top == null)
+            {
+                System.Console.WriteLine("stack is empty");
+                return;
+            }
+            System.Console.WriteLine(this.top.data+"  is the element is top of the stack");
+        }
+
+        public void Pop()    
+        {
+            if (this.top == null)
+            {
+                System.Console.WriteLine("Stack is empty");
+                return;
+            }
+            System.Console.WriteLine(this.top.data+" poped from the stack");
+            this.top = this.top.next;
+        }
+
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            System.Console.WriteLine("Stack is empty now");
+        }
+        
+    }
+}
